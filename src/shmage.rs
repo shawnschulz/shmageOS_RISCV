c3042f419037e4cbd1456addbe4c39741d089f8d
shawnschulz <shawnschulz@gberkeley.edu>
2025-08-25 14:18:05 -0700

    Add extremly basic stack allocated shell with 3 kernel commands

--- src/shmage.rs
+++ src/shmage.rs
@@ -25,7 +25,18 @@ use crate::page;
 pub fn ptable() {
     page::print_page_allocations();
 }
+pub fn clear() {
+    for i in 0..200 {
+        println!();
+    }
+}
 
+/// These use stack allocated memory to check for commands, but this is very limited!
+/// shows why we have a need for heap allocated memory
+/// obviously, we could just allocate from one global heap (arena allocation),
+/// although this would still require letting rust know about the heap
+/// we could also make a macro for this, since the length of the command name is actually known at compile time, but
+/// we will want heap allocation anyways
 pub fn basic_command_process (input_array: &[char; 8]) {
     let shfetch_arr: [char; 7] = ['s', 'h', 'f', 'e', 't', 'c', 'h'];
     let mut shfetch_command: bool = true;
@@ -37,6 +48,7 @@ pub fn basic_command_process (input_array: &[char; 8]) {
     if shfetch_command {
         shfetch();
     }
+
     let ptable_arr: [char; 6] = ['p', 't', 'a', 'b', 'l', 'e'];
     let mut ptable_command: bool = true;
     for i in 0..5 {
@@ -46,6 +58,17 @@ pub fn basic_command_process (input_array: &[char; 8]) {
     }
     if ptable_command {
         ptable();
+    }
+
+    let clear_arr: [char; 5] = ['c', 'l', 'e', 'a', 'r'];
+    let mut clear_command: bool = true;
+    for i in 0..4 {
+        if input_array[i] != clear_arr[i] {
+            clear_command = false;
+        }
+    }
+    if clear_command {
+        clear();
     }
 }
 
@@ -63,7 +86,13 @@ pub fn shmage_init() -> ! {
     let mut input_array: [char; 8] = [' ',' ',' ',' ',' ',' ',' ',' '];
     // single character input process loop
     let mut input_i: usize = 0;
+    // prob eventually want to represent shell state in an enum
+    let mut prompt_active: bool = true;
     loop {
+        if prompt_active {
+            print!("(-_-) — ˎˊ˗");
+            prompt_active = false;
+        }
         // Get the character
         if let Some(c) = uart_instance.get() {
             match c {
@@ -71,6 +100,10 @@ pub fn shmage_init() -> ! {
                     // 8 is the backspace character, need to replace the
                     // previous character with a ' '
                     print!("{}{}{}", 8 as char, ' ', 8 as char);
+                    if input_i > 0 {
+                        input_i - 1;
+                        input_array[input_i] = ' ';
+                    }
                 },
                 10 | 13 => {
                     // carriage returns
@@ -78,6 +111,7 @@ pub fn shmage_init() -> ! {
                     basic_command_process(&input_array);
                     input_array = [' ',' ',' ',' ',' ',' ',' ',' '];
                     input_i = 0;
+                    prompt_active = true;
                 },
                 0x1b => {
                     //ANSI escape sequences
