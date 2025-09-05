784d82b24d58729a4220a05a8e4d4d01c46b5b77
Shawn Schulz <shawnschulz@berkeley.edu>
2025-09-04 22:28:38 -0700

    Fix using wrong nightly version if prev installed

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -2,7 +2,8 @@
 
 # install the prereqs for building the OS from source
 
-rustup default nightly
+rustup toolchain install nightly-2025-07-19-x86_64-unknown-linux-gnu
+rustup default nightly-2025-07-19-x86_64-unknown-linux-gnu
 rustup target add riscv64gc-unknown-none-elf
 cargo install cargo-binutils
 rustup component add rust-analyzer
