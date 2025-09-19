cad06a90552f29468f47994994a2b10729b32d1f
Shawn Schulz <shawnschulz@berkeley.edu>
2025-09-19 15:09:15 -0700

    Fix compiler errors

--- src/malloc.rs
+++ src/malloc.rs
@@ -23,14 +23,14 @@ impl AllocationList {
     pub fn is_taken(&self) -> bool {
         self.flags_size & AllocationFlags::Taken.value() != 0b0
     }
-    pub fn set_taken(&self) {
-        self.flags_size = AllocationFlags::Taken.value() | self.flags_size;
+    pub fn set_taken(&mut self) {
+        self.flags_size |= AllocationFlags::Taken.value();
     }
-    pub fn set_free(&self) {
+    pub fn set_free(&mut self) {
         self.flags_size = !AllocationFlags::Taken.value() & self.flags_size;
     }
     // ngl this makes no sense to me
-    pub fn set_size(&self, size: usize) {
+    pub fn set_size(&mut self, size: usize) {
         let taken_check = self.is_taken();
         self.flags_size = size & !AllocationFlags::Taken.value();
         if taken_check {
@@ -43,14 +43,14 @@ impl AllocationList {
 }
 
 // The head of kernel memory allocation
-static mut KERNEL_MEMORY_HEAD: *mut AllocationList = mull_mut();
+static mut KERNEL_MEMORY_HEAD: *mut AllocationList = null_mut();
 //Track memory footprint to see if more pages need to be allocated to the kernel
 static mut KERNEL_MEMORY_ALLOCATION_SIZE: usize = 0;
-static mut KERNEL_MEMORY_PAGE_TABLE: *mut PageTable = mull_mut();
+static mut KERNEL_MEMORY_PAGE_TABLE: *mut PageTable = null_mut();
 pub fn get_head() -> *mut u8 {
     unsafe {KERNEL_MEMORY_HEAD as *mut u8}
 }
-pub fn get_page_table() -> *mut u8 {
+pub fn get_page_table() -> *mut PageTable {
     unsafe {KERNEL_MEMORY_PAGE_TABLE as *mut PageTable}
 }
 pub fn get_number_allocations() -> usize {
@@ -62,7 +62,7 @@ pub fn init() {
     unsafe {
         // allocate 64 kernel pages
         let kernel_allocation = zalloc(64);
-        assert(!kernel_allocation.is_null())
+        assert!(!kernel_allocation.is_null());
         KERNEL_MEMORY_ALLOCATION_SIZE = 64;
         KERNEL_MEMORY_HEAD = kernel_allocation as *mut AllocationList;
         (*KERNEL_MEMORY_HEAD).set_free();
@@ -71,4 +71,18 @@ pub fn init() {
         KERNEL_MEMORY_PAGE_TABLE = zalloc(1) as *mut PageTable;
 
     }
+}
+
+// allocate memory based on bytes
+pub fn kernel_malloc(size: usize) -> *mut u8 {
+    unsafe {
+       let aligned_size = align_value(size, 3) + size_of::<AllocationList>();
+       let mut head = KERNEL_MEMORY_HEAD;
+    }
+}
+
+// allocate zeroed memory based on number of bytes
+pub fn kernel_zmalloc(size: usize) -> *mut u8 {
+    let aligned_size = align_value(size, 3);
+    let ret = kernel
 }
